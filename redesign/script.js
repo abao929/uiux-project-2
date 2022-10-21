159338dbbac6d7e231063eea16985f38458ea19c
abao929 <44300045+abao929@users.noreply.github.com>
2022-10-21 02:57:52 -0400

    finished woooooooo

--- redesign/script.js
+++ redesign/script.js
@@ -1,38 +1,54 @@
-const body = document.querySelector("body")
-const searchBar = document.querySelector("#search-bar")
-const hamburger = document.querySelector("#hamburger")
-const locateBanner = document.querySelector(".locate-banner")
-const iconBanner = document.querySelector(".icon-banner")
-body.addEventListener("click", (e) => {
-  // console.log(e)
-  // console.log(hamburger.checked)
+const body = document.querySelector('body')
+const searchBar = document.querySelector('#search-bar')
+const hamburger = document.querySelector('#hamburger')
+const locateBanner = document.querySelector('.locate-banner')
+const iconBanner = document.querySelector('.icon-banner')
+const searchResults = document.querySelector('.search-results')
+const chips = Array.from(document.querySelectorAll('.chip'))
+const chipsMenu = document.querySelector('#chip-menu')
+
+body.addEventListener('click', (e) => {
   if (e.target == searchBar) {
-    body.classList.add("blur")
-    body.classList.add("stop-scrolling")
-    for (let img of document.getElementsByTagName("img")) {
-      img.classList.add("test-blur")
-    }
-    document.querySelector(".search-results").classList.remove("hidden")
+    console.log('search')
+    body.classList.add('blur')
+    body.classList.add('stop-scrolling')
+    searchResults.classList.remove('hidden')
     hamburger.checked = false
   } else if (e.target == hamburger) {
-    body.classList.add("blur")
-    body.classList.add("stop-scrolling")
-    // locateBanner.classList.add("lower-z")
-    // iconBanner.classList.add("lower-z")
-    // console.log(hamburger.checked)
+    if (!searchResults.classList.contains('hidden')) {
+      searchResults.classList.add('hidden')
+    }
+    if (chipsMenu.classList.contains('active')) {
+      chipsMenu.classList.remove('active')
+    }
+    if (body.classList.contains('blur')) {
+      body.classList.remove('blur')
+      body.classList.remove('stop-scrolling')
+    } else {
+      body.classList.add('blur')
+      body.classList.add('stop-scrolling')
+    }
+  } else if (chips.includes(e.target)) {
+    body.classList.add('blur')
+    body.classList.add('stop-scrolling')
+    chipsMenu.classList.add('active')
+    hamburger.checked = false
   } else {
-    body.classList.remove("blur")
-    body.classList.remove("stop-scrolling")
-
-    // locateBanner.classList.remove("lower-z")
-    // iconBanner.classList.remove("lower-z")
-    document.querySelector(".search-results").classList.add("hidden")
+    body.classList.remove('blur')
+    body.classList.remove('stop-scrolling')
+    searchResults.classList.add('hidden')
+    chipsMenu.classList.remove('active')
     hamburger.checked = false
   }
-  if (!hamburger.checked) {
-    body.classList.remove("blur")
-    body.classList.remove("stop-scrolling")
-  }
+  // console.log(chips.)
+})
 
-  // console.log(hamburger.checked)
+document.addEventListener('keydown', (e) => {
+  console.log(e)
+  if (e.key == 'Escape') {
+    body.classList.remove('blur')
+    body.classList.remove('stop-scrolling')
+    searchResults.classList.add('hidden')
+    hamburger.checked = false
+  }
 })
